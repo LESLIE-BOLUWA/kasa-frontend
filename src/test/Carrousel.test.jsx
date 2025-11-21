f514c3a2757869e77c32b788fe85452a94be19e5
leslie <lesliedelissi@gmail.com>
2025-11-21 19:15:30 +0100

    import test

--- src/test/Carrousel.test.jsx
+++ src/test/Carrousel.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Carrousel from "./Carrousel";
+import Carrousel from "../components/Carrousel";
 
 const IMAGES = [
   "https://example.com/img1.jpg",
